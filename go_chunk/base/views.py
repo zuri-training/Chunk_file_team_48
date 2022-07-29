08ec32de39bc29476ef11795ee0b9762610b4ea3
Sidney Uwaya <uwayasidney@gmail.com>
2022-07-29 13:51:28 +0100

    update csv file splitting

--- go_chunk/base/views.py
+++ go_chunk/base/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-
+import pandas as pd
 # Create your views here.
 def loginPage(request):
     
@@ -42,3 +42,24 @@ def logoutUser(request):
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+def chunk_file(request):
+    if request.method == "POST":
+        file = request.FILES.get('file')
+        chunk_size = request.POST[chunk_size]
+        
+        if file == None or chunk_size == '' :
+            messages.error(request, 'fields cannot be blank!')
+            return redirect('/')
+        
+        chunk_size = int(chunk_size)
+        batch_no = 1
+        for chunk in pd.read_csv(file, chunksize=chunk_size):
+            new_file = "new_folder/csv_file" + str(batch_no) + ".csv"
+            chunk.to_csv(new_file, index=False)
+            batch_no += 1
+            return redirect('/')
+            
+        
+        
