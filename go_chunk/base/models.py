49b9558a737bdd9aa69156c7e5492579e85f9882
Sidney Uwaya <uwayasidney@gmail.com>
2022-07-30 20:08:44 +0100

    Create function that Splits CSV file #28

--- go_chunk/base/models.py
+++ go_chunk/base/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class ChunkFile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file_id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    file = models.FileField(upload_to=f'files')
