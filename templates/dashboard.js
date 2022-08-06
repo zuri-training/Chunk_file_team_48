c1aa1b975110f00ccbd33a7b428e74e962dd4e1c
jibzsupreme@gmail.com <jibzsupreme@gmail.com>
2022-08-06 11:12:16 +0100

    jibolacodes contribution

--- templates/dashboard.js
+++ templates/dashboard.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.querySelector('.dashboard--nav--toggle--icon');
 const linkDescription = document.querySelectorAll('.dashboard--sidebar--links--desc')
 const linksBar = document.querySelector('.dashboard--sidebar--links');
+const sidebar = document.querySelector('.dashboard--sidebar')
+
 
 toggleBtn.addEventListener('click', function(){
     for (let i = 0; i < linkDescription.length; i++) {
@@ -8,4 +10,9 @@ toggleBtn.addEventListener('click', function(){
         linkDescription[i].classList.toggle('hide');
     }
     linksBar.classList.toggle('.collapse');
+
+    if (window.innerWidth < 1439) {
+        sidebar.classList.toggle('hide')
+    }
 })
+
