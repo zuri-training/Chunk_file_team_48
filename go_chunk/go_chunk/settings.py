bb651b93d9f3fa58facaf4a4e8bc479e125b828d
Gameznut <emmanuelapabiekun@gmail.com>
2022-08-08 23:01:39 +0100

    created the chunk page, display chunked zip file

--- go_chunk/go_chunk/settings.py
+++ go_chunk/go_chunk/settings.py
@@ -16,7 +16,7 @@ from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR= Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
