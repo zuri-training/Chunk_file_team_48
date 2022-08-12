ac690281af35a48fff781efc2734134e6391f1d0
Gameznut <emmanuelapabiekun@gmail.com>
2022-08-12 13:22:30 +0100

    test 7

--- go_chunk/settings.py
+++ go_chunk/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
 # Default primary key field type
--- go_chunk/urls.py
+++ go_chunk/urls.py
@@ -18,12 +18,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf import settings
-from django.conf.urls import url
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("base.urls", namespace="base")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+  static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
