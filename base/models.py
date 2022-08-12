d3d3667f8facca4274930792a28c1e1f6b0cfc15
Gameznut <emmanuelapabiekun@gmail.com>
2022-08-12 15:11:43 +0100

    test 10

--- base/models.py
+++ base/models.py
@@ -4,11 +4,12 @@ import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
 User = get_user_model()
+from django.utils import timezone
 
 
 class ChunkedFile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file_id = models.UUIDField(default=uuid.uuid4, primary_key=True)
-    upload_time = models.DateTimeField(auto_created=True, default=datetime.datetime.now())
+    upload_time = models.DateTimeField(auto_created=True, default=timezone.datetime.now())
     saved = models.BooleanField(default=False)
     file = models.FileField(upload_to=f'files')
